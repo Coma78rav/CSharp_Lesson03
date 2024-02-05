af4cbcaf79705b6b269f2aaf628cf89d5a8582cf
Andrey Rakov <coma78rav@gmail.com>
2024-02-05 23:51:07 +0300

    Lesson03:Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    int numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 3);
+Console.WriteLine(result);
+
